2bf2d6d3a707c13c1c729c055a393d9126ad9aba
JAMES <jameskibunja17@gmail.com>
2023-05-12 11:56:01 +0300

    Assignment Done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :items, only: [:index]
-  resources :users, only: [:show]
+  resources :items, only: [:index]  # for getting all items
+  resources :users do
+    resources :items, only: [:index, :show, :create]  # nested routes
+  end
 end
